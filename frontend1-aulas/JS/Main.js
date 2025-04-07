078cd022aa7ee70aecba1a4c4af61325623a5cff
Tomas4030 <101940482+Tomas4030@users.noreply.github.com>
2025-04-07 21:58:04 +0100

    Add files via upload

---
+++ frontend1-aulas/JS/Main.js
@@ -0,0 +1,25 @@
+//Main Page
+async function fetchJSON() {
+    try {
+        const response = await fetch("./JSON/data.json");
+        const data = await response.json();
+        
+        updateContent("latest_news1", data.latest_news.latest_news1);
+        updateContent("latest_news2", data.latest_news.latest_news2);
+        updateContent("latest_leaks1", data.latest_leaks.latest_leaks1);
+        updateContent("latest_leaks2", data.latest_leaks.latest_leaks2);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function updateContent(prefix, content) {
+    document.getElementById(`${prefix}_title`).textContent = content.title;
+    document.getElementById(`${prefix}_text`).textContent = content.text;
+    document.getElementById(`${prefix}_image`).src = content.image;
+}
+
+fetchJSON();
+
+
+
